493dc619f7b96e32ca9dac2dd9406e21199a2cf4
Shangeri Sivalingam <anushangeri@gmail.com>
2020-12-31 12:38:37 -0500

    added update button

--- src/main/java/net/javatutorial/tutorials/AddVisitorRecordServlet.java
+++ src/main/java/net/javatutorial/tutorials/AddVisitorRecordServlet.java
@@ -64,7 +64,7 @@ public class AddVisitorRecordServlet extends HttpServlet {
 		ArrayList<String> responseObj = new ArrayList<String>();
 		responseObj.add(message + " " + firstName);
 		request.setAttribute("responseObj", responseObj);
-//        RequestDispatcher rd = request.getRequestDispatcher("/vms");
+		// Redirect to view visitor servlet to query all the visitors again.
 		response.sendRedirect("/vms");
 	}
 	@Override
