ef552d5e3e698d842cfd386c011a0ee5e9d40f5f
Shangeri Sivalingam <anushangeri@gmail.com>
2021-01-01 16:10:36 -0500

    add update functionality and retrive visitor by VMS id

--- src/main/java/net/javatutorial/DAO/VMSManagerDAO.java
+++ src/main/java/net/javatutorial/DAO/VMSManagerDAO.java
@@ -57,7 +57,7 @@ public class VMSManagerDAO {
 			stmt = connection.createStatement();
 
 	        stmt.executeUpdate("UPDATE VMS "
-	        		+  "SET TIME_OUT_DT = " + v.getTimeOutDt() +
+	        		+  "SET TIME_OUT_DT = NOW()" +
 	        		"   WHERE VMS_ID = '" + v.getVmsId() + "';");
 	        rs = stmt.executeQuery("SELECT LAST(FIRST_NAME) FROM VMS WHERE VMS_ID ='" + v.getVmsId() +"';");
 	        while (rs.next()) {
